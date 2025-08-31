66722bb6376711658ca23bbd5c9f5bfdf72b6caa
Rajdeep Ramola <rajdeepramola958@gmail.com>
2025-08-31 14:17:46 +0530

    Added Habit tracker project using pixela.

---
+++ Habit_Tracker/main.py
@@ -0,0 +1,67 @@
+import requests
+from datetime import datetime
+
+USERNAME = "YOUR USERNAME"
+TOKEN = "YOUR SELF GENERATED TOKEN"
+GRAPH_ID = "YOUR GRAPH ID"
+
+
+#Create a user.
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+
+
+#Create a graph
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
+
+#Post a pixel
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many kilometers did you cycle today? "),
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+
+#Update a pixel
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+
+
+#Delete a pixel
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+response = requests.delete(url=delete_endpoint, headers=headers)
